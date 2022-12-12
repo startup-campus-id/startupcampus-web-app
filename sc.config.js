0e6b4931f1e640db6dec5ebb971deae2981b0bc4
dapaath <dafaathaullah123@gmail.com>
2022-12-12 15:32:40 +0700

    changing base url if not production

--- sc.config.js
+++ sc.config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://api.startupcampus.id";
+export const BASE_URL = process.env.NODE_ENV === "production" ? "https://api.startupcampus.id" : "http://staging-startupcampus-api.ap-southeast-1.elasticbeanstalk.com";
 export const REGIST_BEASISWA_GOOGLE_URL = "/daftar/beasiswa-gcc";
 export const GCC_URL = "https://grow.google/intl/id_id/certificates/";
